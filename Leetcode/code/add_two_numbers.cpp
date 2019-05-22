b8011da8058d0e0ebf43a741d1d97df27c0100d5
ffskyfan <ffskyfan@gmail.com>
2019-05-22 20:11:56 +0800

    change project to vs2017 and solve addTwoNumbers

--- Leetcode/code/add_two_numbers.cpp
+++ Leetcode/code/add_two_numbers.cpp
@@ -31,8 +31,49 @@ struct ListNode {
 class Solution {
 public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
 
+	  if(l1==nullptr) return l2;
+
+	  bool carry = false;
+	  ListNode* result = l1;
+
+	  while(l1 != nullptr)
+	  {
+		  int l2value = 0;
+		  if(l2 != nullptr) l2value = l2->val;
+
+		  int addResult = l1->val + l2value;
+		  if(addResult / 10 > 0) carry = true;
+
+		  l1->val = addResult % 10;
+
+		  if(carry) {
+			  if(l1->next == nullptr)
+			  {
+				  l1->next = new ListNode(1);
+			  }
+			  else
+			  {
+				  l1->next->val = l1->next->val + 1;
+			  }
+			  carry = false;
+		  }
+
+		  if(l1->next == nullptr)
+		  {
+			  if(l2 != nullptr)
+			  {
+				  l1->next = l2->next;
+			  }
+			  break;
+		  }
+
+		  l1 = l1->next;
+		  if(l2 != nullptr) l2 = l2->next;
+	  }
+
+        
+	  return result;
   }
 
 };
@@ -44,8 +85,12 @@ int main()
 	ListNode* node11 = new ListNode(1);
 	ListNode* node12 = new ListNode(2);
 	node11->next = node12;
-	ListNode* node13 = new ListNode(3);
+	ListNode* node13 = new ListNode(9);
 	node12->next = node13;
+	ListNode* node14 = new ListNode(3);
+	node13->next = node14;
+	ListNode* node15 = new ListNode(3);
+	node14->next = node15;
 
 	ListNode* node21 = new ListNode(8);
 	ListNode* node22 = new ListNode(9);
