aa01806e948eaeee84b401937eff69318e7a3be6
ffskyfan <ffskyfan@gmail.com>
2019-05-23 20:22:49 +0800

    solve "Longest Substring Without Repeating Characters    "

--- Leetcode/code/longest_substring_without_repeating_characters.cpp
+++ Leetcode/code/longest_substring_without_repeating_characters.cpp
@@ -34,14 +34,51 @@ using namespace std;
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
+		size_t count = s.size();
+		if(count<=1) return count;
 
+		size_t subBeginIdx = 0;
+		size_t subEndIdx = 0;
+		int maxLength = 0;
+		for(; subEndIdx < count;)
+		{
+			subEndIdx++;
+
+			char endValue = 0;
+			if(subEndIdx != count) endValue = s[subEndIdx];
+
+			int tempMaxLength = 0;
+			for(size_t i = subBeginIdx; i <= subEndIdx; i++)
+			{
+				if(i == count) break;
+
+				tempMaxLength++;
+
+				if(i == subEndIdx) break;
+
+				if(s[i] == endValue)
+				{
+					subBeginIdx = i + 1;
+					break;
+				}
+			}
+
+
+			if(tempMaxLength > maxLength)
+			{
+				maxLength = tempMaxLength;
+			}
+
+		}
+
+		return maxLength;
 	}
 };
 
 
 int main()
 {
-	string a("abcabcbb");
+	string a("bb");
 
 	Solution so;
 	int result = so.lengthOfLongestSubstring(a);
